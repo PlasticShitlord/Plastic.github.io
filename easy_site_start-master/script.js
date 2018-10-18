934a04ce3add2e0fc0e8408b8fc7824a8d681f19
PlasticShitlord <amatiel98@mail.ru>
2018-10-18 13:33:41 +0300

    18.10.2018

--- easy_site_start-master/script.js
+++ easy_site_start-master/script.js
@@ -2,23 +2,23 @@ new WOW().init();
 var h = document.getElementById('hour');
 var m = document.getElementById('minute');
 var s = document.getElementById('second');
-setInterval(tic,1000);
+setInterval(tick, 1000);
 
-function tic(){
-var d=new Date();
-var hours=23-d.getHours();
-if (hours<10){
-	hours="0"+hours;
-}
-var min=59-d.getMinutes();
-if(min<10){
-	min="0"+min;
-}
-var sec=59-d.getSeconds();
-if(sec<10){
-	sec="0"+sec;
-	}
-h.innerHTML="13:";
-m.innerHTML="13:";
-s.innerHTML="13";
+function tick()
+{
+  var d = new Date;
+  var H = 23 - d.getHours();
+  var M = 59 - d.getMinutes();
+  var S = 59 - d.getSeconds();
+
+  if (H<10)
+  {
+    h.innerHTML = H+":";
+  }
+  else {
+    h.innerHTML = H+":";
+  }
+  h.innerHTML = H+":";
+  m.innerHTML = M+":";
+  s.innerHTML = S;
 }
